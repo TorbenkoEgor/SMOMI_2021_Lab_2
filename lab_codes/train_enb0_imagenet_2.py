43978a9e8600ccf336a5de500ef4b3c13073087c
TorbenkoEgor <58262009+TorbenkoEgor@users.noreply.github.com>
2021-05-10 20:34:36 +0300

    Update train_enb0_imagenet_2.py

--- lab_codes/train_enb0_imagenet_2.py
+++ lab_codes/train_enb0_imagenet_2.py
@@ -56,7 +56,7 @@ def create_dataset(filenames, batch_size):
 
 def build_model():
   inputs = tf.keras.Input(shape=(RESIZE_TO, RESIZE_TO, 3))
-  x = tf.keras.applications.EfficientNetB0(include_top=True, weights=None, classes=NUM_CLASSES)(inputs)
+  x = tf.keras.applications.EfficientNetB0(include_top=True, weights='imagenet', classes=NUM_CLASSES)(inputs)
   x = tf.keras.layers.Flatten()(x)
   outputs = tf.keras.layers.Dense(NUM_CLASSES, activation = tf.keras.activations.softmax)(x)
   return tf.keras.Model(inputs=inputs, outputs=outputs)
